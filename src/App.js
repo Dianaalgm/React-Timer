531c5f39e29fd37ff9e761a2aa555c15672ef72c
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-05-14 13:43:42 -0400

    Initializing state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+//Timer
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
       
-    </div>
-  );
+//     </div>
+//   );
+// }
+
+//Initial State
+const initialState = {
+  seconds: 0,   //---> Initial value for seconds
+  isRunning: false  //---> Initially, the timer will not be running
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'START':
+      return { ...state, isRunning: true}; //--> Start 
+  }
+
 }
+
+
+
+
+
 
 export default App;
